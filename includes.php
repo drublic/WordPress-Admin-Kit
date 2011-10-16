5d17fdec06588997ebd8dcafd077bae82449b6dc
drublic <hans@sa-designz.de>
2011-10-16 20:33:41 +0200

    Add all files in includes.php as this is the only file to include in the theme's functions.php
    Signed-off-by: drublic <hans@sa-designz.de>

---
+++ includes.php
@@ -0,0 +1,19 @@
+<?php
+
+// Get variables with configuration
+require('config.php');
+
+// General Stuff
+require('hooks.php');
+
+// All elements in backend
+require('elements.php');
+
+// Widgets
+require('widgets.php');
+
+// Theme-options
+require('theme-options.php')
+
+
+
