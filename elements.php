e0c2b417b1ff2a4d1e11de8913ebe74d01d0b8f4
drublic <hans@sa-designz.de>
2012-02-13 17:40:42 +0100

    Titles are already moved
    Signed-off-by: drublic <hans@sa-designz.de>

--- elements.php
+++ elements.php
@@ -13,61 +13,6 @@
 
 
 
-
-/**
- *  Generates Output for a title in back-end
- *
- *  @param string $title - title for row
- *  @return html
- */
-function bo_title ($title) {
-  $output =
-    '<tr>
-       <th scope="row" colspan="2">
-         <h2>'.$title.'</h2>
-       </th>
-     </tr>';
-
-  return $output;
-}
-
-
-/**
- *  Generates Output for a small title in back-end
- *
- *  @param string $title - title for row
- *  @return html
- */
-function bo_smalltitle ($title) {
-  $output =
-    '<tr>
-       <th scope="row" colspan="2">
-         <strong>'.$title.'</strong>
-       </th>
-     </tr>';
-
-  return $output;
-}
-
-
-
-
-
-/**
- *  Generates Output for a big title in back-end
- *
- *  @param string $title - title for row
- *  @return html
- */
-function bo_headline ($title) {
-  $output = '<div class="wrap"><h2>'.$title.'</h2></div>';
-
-  return $output;
-}
-
-
-
-
 /**
  *  Generates Output for form headers in back-end
  *
